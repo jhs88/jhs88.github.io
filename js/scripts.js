665ecec841f9924fc5136f4794c320be6f4a848b
Joseph Scherreik <joe.scherreik@gmail.com>
2024-11-09 20:14:06 -0500

    remove jquery for real this time

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
-// window.addEventListener('DOMContentLoaded', function () {
-//   // Activate Bootstrap scrollspy on the main nav element
-//   var sideNav = document.body.querySelector('#sideNav');
-//   if (sideNav)
-//     new this.window.bootstrap.ScrollSpy(document.body, {
-//       target: '#sideNav',
-//       rootMargin: '0px 0px -40%'
-//     });
+window.addEventListener('DOMContentLoaded', function () {
+  // Activate Bootstrap scrollspy on the main nav element
+  var sideNav = document.body.querySelector('#sideNav');
+  if (sideNav)
+    new this.window.bootstrap.ScrollSpy(document.body, {
+      target: '#sideNav',
+      rootMargin: '0px 0px -40%'
+    });
 
-//   // Collapse responsive navbar when toggler is visible
-//   var navbarToggler = document.body.querySelector('.navbar-toggler');
-//   var responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'));
-//   responsiveNavItems.map(function (responsiveNavItem) {
-//     responsiveNavItem.addEventListener('click', function () {
-//       if (window.getComputedStyle(navbarToggler).display !== 'none') navbarToggler.click();
-//     });
-//   });
-// });
+  // Collapse responsive navbar when toggler is visible
+  var navbarToggler = document.body.querySelector('.navbar-toggler');
+  var responsiveNavItems = [].slice.call(document.querySelectorAll('#navbarResponsive .nav-link'));
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener('click', function () {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') navbarToggler.click();
+    });
+  });
+});
 
 window.particlesJS.load('particles-js', 'assets/particlesjs-config.json');
